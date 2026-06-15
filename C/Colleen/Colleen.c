dbc30d55ae76957183316ac9433da79da3f31f52
hle-roux <hle-roux@student.42nice.fr>
2026-06-15 13:16:43 +0200

    first commit - Colleen C

---
+++ C/Colleen/Colleen.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//first
+void quine(void) {}
+int main()
+{
+	char *s = "#include <stdio.h>%1$c//first%1$cvoid quine(void) {}%1$cint main()%1$c{%1$c%2$cchar *s = %3$c%4$s%3$c;%1$c%2$cprintf(s, 10, 9, 34, s);%1$c%2$cquine();%1$c%2$c//second%1$c}%1$c";
+	printf(s, 10, 9, 34, s);
+	quine();
+	//second
+}
