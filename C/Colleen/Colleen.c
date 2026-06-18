d3253cded3768ef891ae3772aabd43170036ffe5
Hugo Le Roux <hle-roux@c1r2p6.42nice.fr>
2026-06-18 18:34:47 +0200

    ASM base

--- C/Colleen/Colleen.c
+++ C/Colleen/Colleen.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /*
-    wrong comment synthax
+	first
 */
 void quine(void) {}
 int main()
 {
-	char *s = "#include <stdio.h>%1$c//first%1$cvoid quine(void) {}%1$cint main()%1$c{%1$c%2$cchar *s = %3$c%4$s%3$c;%1$c%2$cprintf(s, 10, 9, 34, s);%1$c%2$cquine();%1$c%2$c//second%1$c}%1$c";
+	char *s = "#include <stdio.h>%1$c/*%1$c%2$cfirst%1$c*/%1$cvoid quine(void) {}%1$cint main()%1$c{%1$c%2$cchar *s = %3$c%4$s%3$c;%1$c%2$cprintf(s, 10, 9, 34, s);%1$c%2$cquine();%1$c%2$c/*%1$c%2$c%2$csecond%1$c%2$c*/%1$c}%1$c";
 	printf(s, 10, 9, 34, s);
 	quine();
 	/*
-        second
-    */
+		second
+	*/
 }
